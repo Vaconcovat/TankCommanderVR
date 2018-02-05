c65d8a17eff74401f6582bbad1482329a9cef89d
Vaconcovat <vaconcovat@gmail.com>
2018-02-05 20:22:06 +1000

    New camera

---
+++ Assets/Refresher.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using ZenFulcrum.EmbeddedBrowser;
+
+public class Refresher : MonoBehaviour {
+
+	Browser browser;
+
+	// Use this for initialization
+	void Start () {
+		browser = GetComponent<Browser>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		browser.Reload(true);
+	}
+}
